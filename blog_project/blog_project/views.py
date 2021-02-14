40ed0115a42190b88d5877c3f1b7c39843d5e02e
avijit33 <44100793+avijit33@users.noreply.github.com>
2021-02-14 18:04:55 +0600

    Delete blog_project directory

--- blog_project/blog_project/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.urls import reverse
-from django.shortcuts import HttpResponseRedirect
-
-def index(request):
-    return HttpResponseRedirect(reverse('blogapp:blog_list'))
--- blog_project/blogapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from blogapp.models import Blog, Comment
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('comment',)
-        
